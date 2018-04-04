63c8ad03cf1e74a557d34dd3187cc5dcb13e757f
QinChengMaoXian <liruncheng@Runcheng.local>
2018-04-05 00:18:59 +0800

    增加一个生成点阵图的函数。

---
+++ src/util/Util.ts
@@ -0,0 +1,26 @@
+export function buildOrderedDitheringMap(n: number = 4) {
+    const l = Math.pow(2, n);
+    const l2 = l * l;
+    const r = new Array(l2);
+    const cx = [0, 1, 1, 0]; 
+    const cy = [0, 1, 0, 1];
+    const a = [];
+    for (let i = 0; i < n; i++) {
+        a.push(Math.pow(2, n-i-1));
+    }
+    let x, y, b, p;
+    let t = new Uint32Array([0]);
+    for (let i = 0; i < l2; i++) {
+        x = y = 0;
+        t[0] = i;
+        for (let j = 0; j < n; j++) {
+            b = t[0] % 4;
+            p = a[j];
+            x += cx[b] * p;
+            y += cy[b] * p;
+            t[0] /= 4;
+        }
+        r[y * l + x] = i;
+    }
+    return r;
+}
