da1fa0e4a42125db0f43300c0df99e76d5af138c
QinChengMaoXian <liruncheng@Runcheng.lan>
2019-03-14 22:46:17 +0800

    位置现在正确了，准备修正深度的问题

--- src/graphics/TextureCube.ts
+++ src/graphics/TextureCube.ts
@@ -8,7 +8,7 @@ export const CubeVectors = [
     { target: new Vector3(-1.0,  0.0,  0.0), up: new Vector3(0.0,  0.0, -1.0) },
     { target: new Vector3( 0.0,  0.0,  1.0), up: new Vector3(0.0, -1.0, 0.0) },
     { target: new Vector3( 0.0,  0.0, -1.0), up: new Vector3(0.0,  1.0, 0.0) },
-    { target: new Vector3( 0.0, -1.0,  0.0), up: new Vector3(0.0,  0.0, 1.0) },
+    { target: new Vector3( 0.0, -1.0,  0.0), up: new Vector3(0.0,  0.0, -1.0) },
     { target: new Vector3( 0.0,  1.0,  0.0), up: new Vector3(0.0,  0.0, -1.0) },
 ];
 
